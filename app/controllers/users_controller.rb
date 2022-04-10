925ccb15857e5319d9f0777e98fc2d507a7d44f7
alexander gubernikoff <agubernikoff@gmail.com>
2022-04-10 15:12:29 -0400

    completed assignment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+# rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+before_action :authorize, only: [:show]
+
+  
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user
+        else render json: {error: user.errors.full_messages}, status: :unprocessable_entity 
+        end
+    end
+
+    def show
+        user = User.find(session[:user_id])
+        render json: user
+    end
+    
+    private
+
+    def user_params
+        params.permit(:username,:password,:password_confirmation)
+    end
+
+    def authorize
+        render json: {error: "Not Authorized"}, status: :unauthorized unless session.include? :user_id
+      end
+end
